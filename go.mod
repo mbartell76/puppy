fc0e34420bbc2e02a5b83a655ddd7cac5dbbf130
mbartell76 <31777935+mbartell76@users.noreply.github.com>
2024-07-28 18:23:24 -0500

    updated puppy to add dog dependency

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mbartell76/puppy
 
 go 1.22.5
+
+require github.com/mbartell76/dog v0.0.0-20240728231519-f6988a920b2a
--- puppy.go
+++ puppy.go
@@ -1,9 +1,23 @@
 package puppy
 
+import (
+	"github.com/mbartell76/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
+
 }
